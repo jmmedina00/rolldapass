34d3286b352b06f016abc54a9d6fb2cb5c1df162
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-15 20:25:48 +0200

    chore: test MUI library is working correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, CardContent, Typography } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Card sx={{ maxWidth: 400 }}>
+    <CardContent>
+      <Typography variant="h3" gutterBottom>
+        This is a test
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Testing MUI library
+      </Typography>
+    </CardContent>
+  </Card>
+);
 
 export default App;

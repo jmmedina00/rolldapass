ddb404d948b24a3856bd9a73fe7c0339acdee7f2
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-19 19:33:01 +0200

    chore: plug config slice into store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import configReducer from "../features/pass-config/configSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { config: configReducer },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself

a4d3f4e12fe47b8baee2cd757d3e47ef1bfb270c
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-19 13:23:59 +0200

    feat(PassGenerator): animate entropy selector

--- src/feats/pass-generator/components/PassEntropy.tsx
+++ src/feats/pass-generator/components/PassEntropy.tsx
@@ -5,36 +5,53 @@ import {
   LinearProgress,
   MenuItem,
   Select,
+  SelectChangeEvent,
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 
-const PassEntropy = () => (
-  <Box>
-    <LinearProgress
-      variant="determinate"
-      value={78}
-      sx={{ height: 8, borderRadius: 4 }}
-    />
-    <Typography variant="overline" align="right" paragraph={true}>
-      Entropy: 78 bit
-    </Typography>
-    <Grid sx={{ textAlign: "right" }}>
-      <FormControl>
-        <InputLabel>Entropy algorythm</InputLabel>
-        <Select
-          label="Entropy algorythm"
-          variant="outlined"
-          value={"zxcvbn"}
-          sx={{ minWidth: 300, textAlign: "left" }}
-        >
-          <MenuItem value="zxcvbn">zxcvbn</MenuItem>
-          <MenuItem value="uic">UIC</MenuItem>
-          <MenuItem value="tai">Tai-Password-Strength</MenuItem>
-        </Select>
-      </FormControl>
-    </Grid>
-  </Box>
-);
+const entropyList = {
+  zxcvbn: "zxcvbn",
+  uic: "UIC",
+  tai: "Tai-Password-Strength",
+};
+
+const PassEntropy = () => {
+  const [entropy, setEntropy] = useState<string>("zxcvbn");
+
+  const handleEntropyChange = (event: SelectChangeEvent) => {
+    setEntropy(event.target.value);
+  };
+
+  return (
+    <Box>
+      <LinearProgress
+        variant="determinate"
+        value={78}
+        sx={{ height: 8, borderRadius: 4 }}
+      />
+      <Typography variant="overline" align="right" paragraph={true}>
+        Entropy: 78 bit
+      </Typography>
+      <Grid sx={{ textAlign: "right" }}>
+        <FormControl>
+          <InputLabel>Entropy algorythm</InputLabel>
+          <Select
+            label="Entropy algorythm"
+            variant="outlined"
+            value={entropy}
+            onChange={handleEntropyChange}
+            sx={{ minWidth: 300, textAlign: "left" }}
+          >
+            {Object.entries(entropyList).map(([key, label]) => (
+              <MenuItem value={key}>{label}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+    </Box>
+  );
+};
 
 export default PassEntropy;

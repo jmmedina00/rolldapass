c1ad6954077bd86f28dfca48800758f02f33bdfa
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-19 12:59:45 +0200

    feat(PassConfig): make charset chips toggleable

--- src/feats/pass-config/components/CharsetChip.tsx
+++ src/feats/pass-config/components/CharsetChip.tsx
@@ -1,12 +1,25 @@
 import { Chip, Grid } from "@mui/material";
+import { useState } from "react";
 import { CharsetDefinitionProperty } from "../../../types/CharsetDefinition";
 
 const CharsetChip = ({
   charsetDef: { label, charset },
-}: CharsetDefinitionProperty) => (
-  <Grid item>
-    <Chip label={label} color="default" />
-  </Grid>
-);
+}: CharsetDefinitionProperty) => {
+  const [enabled, setEnabled] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setEnabled(!enabled);
+  };
+
+  return (
+    <Grid item>
+      <Chip
+        label={label}
+        color={enabled ? "primary" : "default"}
+        onClick={handleClick}
+      />
+    </Grid>
+  );
+};
 
 export default CharsetChip;

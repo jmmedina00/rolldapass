1041b270b8c85d2f6da39b9570ca3d6542fae169
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-24 13:47:53 +0200

    feat(pass-generator): define password slice

---
+++ src/features/pass-generator/passwordSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface PasswordState {
+  password: string;
+}
+
+const initialState: PasswordState = {
+  password: "",
+};
+
+export const passwordSlice = createSlice({
+  name: "password",
+  initialState,
+  reducers: {
+    changePassword: (state: PasswordState, action: PayloadAction<string>) => ({
+      ...state,
+      password: action.payload,
+    }),
+  },
+});
+
+export const { changePassword } = passwordSlice.actions;
+export default passwordSlice.reducer;

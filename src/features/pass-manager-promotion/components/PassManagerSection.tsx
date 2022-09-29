ee575b438ef17ab104ef701175736f83e582d0e9
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-29 20:36:28 +0200

    feat(pass-manager-promotion): create visual section

---
+++ src/features/pass-manager-promotion/components/PassManagerSection.tsx
@@ -0,0 +1,23 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { PromotionSection } from "../constants";
+import PassManagerCard from "./PassManagerCard";
+
+const PassManagerSection = ({
+  section: { label, items },
+}: {
+  section: PromotionSection;
+}) => {
+  const passManagerCards = items.map((info) => <PassManagerCard info={info} />);
+
+  return (
+    <Box>
+      <Typography variant="h6">{label}</Typography>
+      <Grid container spacing={3}>
+        {passManagerCards}
+      </Grid>
+    </Box>
+  );
+};
+
+export default PassManagerSection;

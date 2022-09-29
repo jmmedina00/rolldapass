49691fd979d9065ca69e19ec0233b6b9d7d65759
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-29 20:48:00 +0200

    chore(pass-manager-promotion): add flat cards back to main promotion

--- src/features/pass-manager-promotion/components/PassManagerPromotion.tsx
+++ src/features/pass-manager-promotion/components/PassManagerPromotion.tsx
@@ -1,11 +1,16 @@
 import { Box } from "@mui/material";
 import { passManagers } from "../constants";
+import PassManagerCard from "./PassManagerCard";
 import PassManagerSection from "./PassManagerSection";
 
 const PassManagerPromotion = () => {
   const sections = Object.entries(passManagers).map(([key, section]) => (
     <PassManagerSection key={key} section={section} />
   ));
+
+  const flatCards = Object.entries(passManagers)
+    .flatMap(([_, section]) => section.items)
+    .map((info) => <PassManagerCard key={info.name} info={info} />);
 
   return <Box>{sections}</Box>;
 };

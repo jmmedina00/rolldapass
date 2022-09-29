dab0756268111b99ff4ba1584626a4fae676a45c
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-09-29 20:11:57 +0200

    feat(pass-manager-promotion): separate info card into component

---
+++ src/features/pass-manager-promotion/components/PassManagerInfo.tsx
@@ -0,0 +1,28 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { PassManagerInfo } from "../constants";
+
+const PassManagerInfo = ({
+  info: { name, description, image, url },
+}: {
+  info: PassManagerInfo;
+}) => (
+  <Grid item xs={3}>
+    <Card>
+      <CardMedia component="img" image={image} height="auto" />
+      <CardContent sx={{ paddingBottom: 0 }}>
+        <Typography variant="h6">{name}</Typography>
+        <Typography variant="body2">{description}</Typography>
+        <Button href={url} fullWidth>
+          Go to website
+        </Button>
+      </CardContent>
+    </Card>
+  </Grid>
+);

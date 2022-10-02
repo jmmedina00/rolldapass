68571f7e838d3ac04bb362be0f7b35403e5253a6
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-02 17:55:13 +0200

    chore: add the rest of entropy algorithms

--- src/features/pass-generator/services/entropy.ts
+++ src/features/pass-generator/services/entropy.ts
@@ -1,6 +1,11 @@
 import { PasswordMeter } from "password-meter";
+import { PasswordStrength } from "tai-password-strength";
+import PasswordQualityCalculator from "password-quality-calculator";
+import zxcvbn from "zxcvbn";
 
 const passwordMeter = new PasswordMeter();
+const taiStrength = new PasswordStrength();
+const taiCodes = ["VERY_WEAK", "WEAK", "REASONABLE", "STRONG", "VERY_STRONG"];
 
 export interface EntropyResult {
   strengthPercent: number;
@@ -23,3 +28,51 @@ const uic: EntropyAlgorithm = {
     };
   },
 };
+
+const shannon: EntropyAlgorithm = {
+  label: "TAI Shannon",
+  calculator: (password) => {
+    const result = taiStrength.check(password);
+
+    return {
+      strengthPercent:
+        (taiCodes.indexOf(result.strengthCode) / (taiCodes.length - 1)) * 100,
+      info: `Entropy: ${result.shannonEntropyBits} bits`,
+    };
+  },
+};
+
+const keepass: EntropyAlgorithm = {
+  label: "KeePass",
+  calculator: (password) => {
+    const entropy = PasswordQualityCalculator(password);
+
+    return {
+      strengthPercent: (entropy / 128) * 100,
+      info: `Entropy: ${entropy} bits`,
+    };
+  },
+};
+
+const zxcvbnAlgorithm: EntropyAlgorithm = {
+  label: "zxcvbn",
+  calculator: (password) => {
+    const result = zxcvbn(password);
+
+    return {
+      info: `Strength: ${result.guesses_log10}`,
+      strengthPercent: (result.score / 4) * 100,
+    };
+  },
+};
+
+const algorithms: { [key: string]: EntropyAlgorithm } = {
+  uic,
+  shannon,
+  keepass,
+  zxcvbn: zxcvbnAlgorithm,
+};
+
+const list = Object.keys(algorithms);
+
+export default { algorithms, list };

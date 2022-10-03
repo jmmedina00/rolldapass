992fb4c81ed5a1a58bb22babefa7a8de4002344d
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-03 18:22:11 +0200

    chore: add pass health reducer to store, save it to local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,15 +7,17 @@ import {
 import { save } from "redux-localstorage-simple";
 import configReducer from "../features/pass-config/configSlice";
 import passwordReducer from "../features/pass-generator/passwordSlice";
+import passHealthReducer from "../features/pass-generator/passHealthSlice";
 import settingsReducer from "../features/appbar-settings/settingsSlice";
 
 const rootReducer = combineReducers({
   config: configReducer,
   passwordGenerator: passwordReducer,
+  passwordHealth: passHealthReducer,
   settings: settingsReducer,
 });
 
-export const saveOptions = { states: ["config", "settings"] };
+export const saveOptions = { states: ["config", "passwordHealth", "settings"] };
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
   configureStore({

dc6b6282ab52bf51b2a1e73aefe2f7eba9d2a4c6
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-03 18:34:51 +0200

    test(pass-generator): adapt pass entropy test to accomodate for entropy state

--- src/features/pass-generator/components/PassEntropy.spec.tsx
+++ src/features/pass-generator/components/PassEntropy.spec.tsx
@@ -5,28 +5,36 @@ import { setupStore } from "../../../app/store";
 import { changePassword } from "../passwordSlice";
 import { act } from "react-dom/test-utils";
 import { fireEvent } from "@testing-library/react";
+import { changeEntropy } from "../passHealthSlice";
 
 describe("pass entropy", () => {
   const password = "1234qwer";
+  const defaultEntropy = "zxcvbn";
   it("should calculate entropy of current password", () => {
     const { getByText } = renderWithProviders(<PassEntropy />, {
-      preloadedState: { passwordGenerator: { password } },
+      preloadedState: {
+        passwordGenerator: { password },
+        passwordHealth: { entropy: defaultEntropy },
+      },
     });
 
-    const expected = entropy.algorithms["zxcvbn"].calculator(password);
+    const expected = entropy.algorithms[defaultEntropy].calculator(password);
     expect(getByText(expected.info)).toBeInTheDocument();
     // Would love to test progress bar if I knew how...
   });
 
   it("should refresh when password changes", () => {
-    const store = setupStore({ passwordGenerator: { password } });
+    const store = setupStore({
+      passwordGenerator: { password },
+      passwordHealth: { entropy: defaultEntropy },
+    });
 
     const { getByText } = renderWithProviders(<PassEntropy />, {
       store,
     });
 
     const newPassword = "2Agf%HAy^cWgA5zyhbU";
-    const expected = entropy.algorithms["zxcvbn"].calculator(newPassword);
+    const expected = entropy.algorithms[defaultEntropy].calculator(newPassword);
 
     act(() => {
       store.dispatch(changePassword(newPassword));
@@ -35,19 +43,40 @@ describe("pass entropy", () => {
     expect(getByText(expected.info)).toBeInTheDocument();
   });
 
-  it("should refresh when selected entropy changes", () => {
-    const { getByText, getByDisplayValue } = renderWithProviders(
-      <PassEntropy />,
-      {
-        preloadedState: { passwordGenerator: { password } },
-      }
-    );
+  it("should change selected entropy in store", () => {
+    const expectedStore = setupStore({
+      passwordHealth: { entropy: defaultEntropy },
+    });
+    expectedStore.dispatch(changeEntropy("uic"));
 
-    const expected = entropy.algorithms["uic"].calculator(password);
-    const selector = getByDisplayValue("zxcvbn") as HTMLInputElement;
+    const actualStore = setupStore({
+      passwordHealth: { entropy: defaultEntropy },
+    });
+
+    const { getByDisplayValue } = renderWithProviders(<PassEntropy />, {
+      store: actualStore,
+    });
+    const selector = getByDisplayValue(defaultEntropy) as HTMLInputElement;
 
     act(() => {
       fireEvent.change(selector, { target: { value: "uic" } });
+    });
+
+    expect(expectedStore.getState().passwordHealth.entropy).toEqual(
+      actualStore.getState().passwordHealth.entropy
+    );
+  });
+
+  it("should refresh when selected entropy changes", () => {
+    const store = setupStore({ passwordHealth: { entropy: defaultEntropy } });
+    const { getByText } = renderWithProviders(<PassEntropy />, {
+      store,
+    });
+
+    const expected = entropy.algorithms["uic"].calculator(password);
+
+    act(() => {
+      store.dispatch(changeEntropy("uic"));
     });
 
     expect(getByText(expected.info)).toBeInTheDocument();

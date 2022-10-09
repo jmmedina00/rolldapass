67556120f7ff6c4e018bebe5c58b747c1ea9ef5a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-09 17:40:22 +0200

    feat(pass-generator): implement password generation service

--- src/features/pass-generator/services/generate-password.ts
+++ src/features/pass-generator/services/generate-password.ts
@@ -1,2 +1,16 @@
-export const generatePassword = (length: number, charsets: string[]): string =>
-  "";
+export const generatePassword = (
+  length: number,
+  charsets: string[]
+): string => {
+  const charsetChoices = crypto
+    .getRandomValues(new Uint32Array(length))
+    .map((n) => n % charsets.length);
+
+  const charChoices = crypto
+    .getRandomValues(new Uint32Array(length))
+    .map((value, index) => value % charsets[charsetChoices[index]].length);
+
+  return Array.from(charsetChoices)
+    .map((choice, index) => charsets[choice][charChoices[index]])
+    .join("");
+};

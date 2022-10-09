95aed85531c9a6e72deeb0f5820ca813b16641e9
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-09 19:30:23 +0200

    refactor(pass-generator): reimplement password generation

--- src/features/pass-generator/services/generate-password.ts
+++ src/features/pass-generator/services/generate-password.ts
@@ -2,15 +2,36 @@ export const generatePassword = (
   length: number,
   charsets: string[]
 ): string => {
-  const charsetChoices = crypto
-    .getRandomValues(new Uint32Array(length))
-    .map((n) => n % charsets.length);
+  let password = "";
 
-  const charChoices = crypto
-    .getRandomValues(new Uint32Array(length))
-    .map((value, index) => value % charsets[charsetChoices[index]].length);
+  const charsCombined = charsets.reduce(
+    (collection, charset) => collection + charset,
+    ""
+  );
 
-  return Array.from(charsetChoices)
-    .map((choice, index) => charsets[choice][charChoices[index]])
-    .join("");
+  do {
+    const generatedPassword = Array.from(
+      crypto.getRandomValues(new Uint32Array(length))
+    )
+      .map((n) => charsCombined[n % charsCombined.length])
+      .join("");
+
+    const charsetsFirstFound = charsets.map((charset) =>
+      charset
+        .split("")
+        .findIndex((char) => generatedPassword.indexOf(char) !== -1)
+    );
+
+    const missingCharsetsInPassword =
+      charsetsFirstFound.filter((indexFound) => indexFound !== -1).length !==
+      charsets.length;
+
+    if (length > charsets.length && missingCharsetsInPassword) {
+      continue;
+    }
+
+    password = generatedPassword;
+  } while (password === "");
+
+  return password;
 };

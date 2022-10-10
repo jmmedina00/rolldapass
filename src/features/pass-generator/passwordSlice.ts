4a8fe0215461d69402514c6be6192ccd45ff6eab
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-10 19:46:22 +0200

    feat(pass-generator): add thunks for handling clipboard

--- src/features/pass-generator/passwordSlice.ts
+++ src/features/pass-generator/passwordSlice.ts
@@ -1,11 +1,19 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  AnyAction,
+  createSlice,
+  PayloadAction,
+  ThunkAction,
+} from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
 
 export interface PasswordState {
   password: string;
+  copiedTimeout: number | undefined;
 }
 
 const initialState: PasswordState = {
   password: "",
+  copiedTimeout: undefined,
 };
 
 export const passwordSlice = createSlice({
@@ -16,8 +24,41 @@ export const passwordSlice = createSlice({
       ...state,
       password: action.payload,
     }),
+    setTimeoutId: (state: PasswordState, action: PayloadAction<number>) => ({
+      ...state,
+      copiedTimeout: action.payload,
+    }),
+    clearTimeoutId: (state: PasswordState) => ({
+      ...state,
+      copiedTimeout: undefined,
+    }),
   },
 });
 
-export const { changePassword } = passwordSlice.actions;
+export const clearClipboard =
+  (): ThunkAction<void, RootState, unknown, AnyAction> => async (dispatch) => {
+    await window.navigator.clipboard.writeText("");
+    dispatch(clearTimeoutId());
+  };
+
+export const copyPasswordToClipboard =
+  (): ThunkAction<void, RootState, unknown, AnyAction> =>
+  async (dispatch, getState) => {
+    const { password, copiedTimeout } = getState().passwordGenerator;
+
+    if (copiedTimeout) {
+      dispatch(clearTimeoutId());
+    }
+
+    await window.navigator.clipboard.writeText(password);
+
+    const newTimeout = setTimeout(() => {
+      dispatch(clearClipboard());
+    }, 10000);
+
+    dispatch(setTimeoutId(newTimeout as unknown as number));
+  };
+
+export const { changePassword, setTimeoutId, clearTimeoutId } =
+  passwordSlice.actions;
 export default passwordSlice.reducer;

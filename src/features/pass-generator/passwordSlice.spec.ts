dc61b778ea973cff366b7d736ae24e62fed075bc
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-10 19:52:57 +0200

    test(pass-generator): add tests for password slice reducers

--- src/features/pass-generator/passwordSlice.spec.ts
+++ src/features/pass-generator/passwordSlice.spec.ts
@@ -1,15 +1,28 @@
-import passwordReducer, { changePassword } from "./passwordSlice";
+import passwordReducer, { changePassword, setTimeoutId } from "./passwordSlice";
 
 describe("password reducer", () => {
   it("should handle initial state", () => {
     expect(passwordReducer(undefined, { type: "???" })).toEqual({
       password: "",
+      copiedTimeout: undefined,
     });
   });
 
   it("should handle password change", () => {
-    expect(passwordReducer({ password: "old" }, changePassword("new"))).toEqual(
-      { password: "new" }
-    );
+    expect(
+      passwordReducer(
+        { password: "old", copiedTimeout: undefined },
+        changePassword("new")
+      )
+    ).toEqual({ password: "new", copiedTimeout: undefined });
+  });
+
+  it("should handle copy timeout setting", () => {
+    expect(
+      passwordReducer(
+        { password: "old", copiedTimeout: undefined },
+        setTimeoutId(12)
+      )
+    ).toEqual({ password: "old", copiedTimeout: 12 });
   });
 });

4ed3a0c5c7e9cf3e64c286b9bff1048208c97ba0
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-22 18:13:35 +0200

    feat(pass-generator): implement HIBP password query logic

--- src/features/pass-generator/services/haveIBeenPwned.ts
+++ src/features/pass-generator/services/haveIBeenPwned.ts
@@ -1,2 +1,22 @@
-export const checkPassword = async (password: string): Promise<boolean> =>
-  false;
+import { SHA1 } from "crypto-js";
+
+export const checkPassword = async (password: string): Promise<boolean> => {
+  const passwordHash = SHA1(password).toString();
+  const [_, hashStart] = passwordHash.match(/^(.{5}).+$/) as string[];
+
+  const response = await fetch(
+    `https://api.pwnedpasswords.com/range/${hashStart}`
+  );
+  const result = await response.text();
+
+  const resultingHashes = result
+    .split("\r\n")
+    .map((line) => {
+      const [halfHash, occurrences] = line.split(":");
+      return { hash: hashStart + halfHash, occurrences: +occurrences };
+    })
+    .filter(({ occurrences }) => occurrences > 0)
+    .map(({ hash }) => hash.toLowerCase());
+
+  return !!resultingHashes.find((hash) => hash === passwordHash);
+};

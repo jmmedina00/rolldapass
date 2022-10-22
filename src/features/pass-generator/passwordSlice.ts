d3e1af1a384bbd035145d015ebf782a91e8a2002
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-22 19:35:13 +0200

    feat(pass-generator): add pwned thunk and reducers to password slice

--- src/features/pass-generator/passwordSlice.ts
+++ src/features/pass-generator/passwordSlice.ts
@@ -1,14 +1,22 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { checkPassword } from "./services/haveIBeenPwned";
 
 export interface PasswordState {
   password: string;
   copiedTimeout: number | undefined;
+  pwnedResult: "none" | "pending" | "good" | "bad" | "error";
 }
 
 const initialState: PasswordState = {
   password: "",
   copiedTimeout: undefined,
+  pwnedResult: "none",
 };
+
+export const checkIfPwned = createAsyncThunk(
+  "passwordGenerator/checkIfPwned",
+  checkPassword
+);
 
 export const passwordSlice = createSlice({
   name: "passwordGenerator",
@@ -26,9 +34,29 @@ export const passwordSlice = createSlice({
       ...state,
       copiedTimeout: undefined,
     }),
+    resetPwned: (state: PasswordState) =>
+      ({ ...state, pwnedResult: "none" } as PasswordState),
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(checkIfPwned.pending, (state: PasswordState) => ({
+        ...state,
+        pwnedResult: "pending",
+      }))
+      .addCase(
+        checkIfPwned.fulfilled,
+        (state: PasswordState, action: PayloadAction<boolean>) => ({
+          ...state,
+          pwnedResult: action.payload ? "bad" : "good",
+        })
+      )
+      .addCase(checkIfPwned.rejected, (state: PasswordState) => ({
+        ...state,
+        pwnedResult: "error",
+      }));
   },
 });
 
-export const { changePassword, setTimeoutId, clearTimeoutId } =
+export const { changePassword, setTimeoutId, clearTimeoutId, resetPwned } =
   passwordSlice.actions;
 export default passwordSlice.reducer;

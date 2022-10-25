acb2977f88d2b9361d632bce6d590911724fb22f
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-25 15:47:55 +0200

    refactor(pass-generator): move charset normalization into a selector

---
+++ src/features/pass-generator/selectNormalizedCharsets.ts
@@ -0,0 +1,46 @@
+import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import { Settings } from "../appbar-settings/constants";
+import { ConfigState } from "../pass-config/configSlice";
+
+const selectIsAdvanced = (state: RootState): boolean =>
+  state.settings.toggle[Settings.AdvancedConfig];
+const selectConfig = (state: RootState): Omit<ConfigState, "length"> => {
+  const { charsets, additionalChars } = state.config;
+  return { charsets, additionalChars };
+};
+
+export const selectNormalizedCharsets = createSelector(
+  [selectIsAdvanced, selectConfig],
+  (
+    isAdvanced: boolean,
+    { charsets: { basic, advanced }, additionalChars: { include, exclude } }
+  ): string[] => {
+    if (!isAdvanced) {
+      return [...basic];
+    }
+
+    const baseCharsets = [...basic, ...advanced];
+    const sanitizedInclude = Array.from(
+      new Set(
+        include
+          .split("")
+          .filter(
+            (char) =>
+              baseCharsets.findIndex(
+                (charset) => charset.indexOf(char) !== -1
+              ) === -1
+          )
+      )
+    ).join("");
+
+    return [...baseCharsets, sanitizedInclude]
+      .map((charset) =>
+        charset
+          .split("")
+          .filter((char) => exclude.indexOf(char) === -1)
+          .join("")
+      )
+      .filter((charset) => charset !== "");
+  }
+);

5620b2156330d49d8827a63489dc1a22b777447d
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-27 12:28:30 +0200

    chore(pass-manager-promotion): get cards displaying at a proper size in all situations

--- src/features/pass-manager-promotion/components/PassManagerCard.tsx
+++ src/features/pass-manager-promotion/components/PassManagerCard.tsx
@@ -6,6 +6,7 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import { useAppSelector } from "../../../app/hooks";
 import { PassManagerInfo } from "../constants";
 
 const PassManagerCard = ({
@@ -13,19 +14,23 @@ const PassManagerCard = ({
   description,
   image,
   url,
-}: PassManagerInfo) => (
-  <Grid item xs={3}>
-    <Card>
-      <CardMedia component="img" image={image} height="auto" />
-      <CardContent sx={{ paddingBottom: 0 }}>
-        <Typography variant="h6">{name}</Typography>
-        <Typography variant="body2">{description}</Typography>
-        <Button href={url} fullWidth>
-          Go to website
-        </Button>
-      </CardContent>
-    </Card>
-  </Grid>
-);
+}: PassManagerInfo) => {
+  const isSettingsOpen = useAppSelector((state) => state.settings.settingsOpen);
+
+  return (
+    <Grid item lg={3} md={isSettingsOpen ? 6 : 3} xs={6}>
+      <Card>
+        <CardMedia component="img" image={image} height="auto" />
+        <CardContent sx={{ paddingBottom: 0 }}>
+          <Typography variant="h6">{name}</Typography>
+          <Typography variant="body2">{description}</Typography>
+          <Button href={url} fullWidth>
+            Go to website
+          </Button>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
 
 export default PassManagerCard;

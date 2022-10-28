33937945ee18847c62f5747829dd11c54415246c
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-28 12:06:47 +0200

    test(pass-config): reorganize tests for pass config

--- src/features/pass-config/components/PassConfig.spec.tsx
+++ src/features/pass-config/components/PassConfig.spec.tsx
@@ -5,59 +5,63 @@ import PassConfig from "./PassConfig";
 jest.mock("./CharFieldSet");
 
 describe("pass configuration", () => {
-  it("should display basic charset config", () => {
-    const { getByText } = renderWithProviders(<PassConfig />, {
-      preloadedState: {
-        settings: {
-          settingsOpen: false,
-          aboutOpen: false,
-          toggle: { [Settings.AdvancedConfig]: false },
+  describe("when advanced config is false", () => {
+    it("should display basic charset config", () => {
+      const { getByText } = renderWithProviders(<PassConfig />, {
+        preloadedState: {
+          settings: {
+            settingsOpen: false,
+            aboutOpen: false,
+            toggle: { [Settings.AdvancedConfig]: false },
+          },
         },
-      },
+      });
+
+      expect(getByText("Uppercase (A-Z)")).toBeInTheDocument();
     });
 
-    expect(getByText("Uppercase (A-Z)")).toBeInTheDocument();
+    it("should not display include/exclude", () => {
+      const { queryByText } = renderWithProviders(<PassConfig />, {
+        preloadedState: {
+          settings: {
+            settingsOpen: false,
+            aboutOpen: false,
+            toggle: { [Settings.AdvancedConfig]: false },
+          },
+        },
+      });
+
+      expect(queryByText("Include/exclude active")).toBeNull();
+    });
   });
 
-  it("should display advanced charset config", () => {
-    const { getByText } = renderWithProviders(<PassConfig />, {
-      preloadedState: {
-        settings: {
-          settingsOpen: false,
-          aboutOpen: false,
-          toggle: { [Settings.AdvancedConfig]: true },
+  describe("when advanced config is true", () => {
+    it("should display advanced charset config", () => {
+      const { getByText } = renderWithProviders(<PassConfig />, {
+        preloadedState: {
+          settings: {
+            settingsOpen: false,
+            aboutOpen: false,
+            toggle: { [Settings.AdvancedConfig]: true },
+          },
         },
-      },
+      });
+
+      expect(getByText("A-Z")).toBeInTheDocument();
     });
 
-    expect(getByText("A-Z")).toBeInTheDocument();
-  });
-
-  it("should display include/exclude when advanced config is active", () => {
-    const { getByText } = renderWithProviders(<PassConfig />, {
-      preloadedState: {
-        settings: {
-          settingsOpen: false,
-          aboutOpen: false,
-          toggle: { [Settings.AdvancedConfig]: true },
+    it("should display include/exclude", () => {
+      const { getByText } = renderWithProviders(<PassConfig />, {
+        preloadedState: {
+          settings: {
+            settingsOpen: false,
+            aboutOpen: false,
+            toggle: { [Settings.AdvancedConfig]: true },
+          },
         },
-      },
+      });
+
+      expect(getByText("Include/exclude active")).toBeInTheDocument();
     });
-
-    expect(getByText("Include/exclude active")).toBeInTheDocument();
-  });
-
-  it("should not display include/exclude when advanced config is inactive", () => {
-    const { queryByText } = renderWithProviders(<PassConfig />, {
-      preloadedState: {
-        settings: {
-          settingsOpen: false,
-          aboutOpen: false,
-          toggle: { [Settings.AdvancedConfig]: false },
-        },
-      },
-    });
-
-    expect(queryByText("Include/exclude active")).toBeNull();
   });
 });

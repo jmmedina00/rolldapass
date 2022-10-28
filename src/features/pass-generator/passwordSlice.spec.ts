ab855aee261cbb7af3c65bb64a28699a6ce9bdc8
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-28 12:13:12 +0200

    test(pass-generator): reorganize tests for password slice

--- src/features/pass-generator/passwordSlice.spec.ts
+++ src/features/pass-generator/passwordSlice.spec.ts
@@ -27,66 +27,70 @@ describe("password reducer", () => {
     });
   });
 
-  it("should handle copy timeout setting", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: undefined, pwnedResult: "none" },
-        setTimeoutId(12)
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "none" });
+  describe("copy timeout id", () => {
+    it("should handle setting", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: undefined, pwnedResult: "none" },
+          setTimeoutId(12)
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "none" });
+    });
+
+    it("should handle clearing", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "none" },
+          clearTimeoutId()
+        ).copiedTimeout
+      ).toBeFalsy();
+    });
   });
 
-  it("should handle copy timeout clearing", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "none" },
-        clearTimeoutId()
-      ).copiedTimeout
-    ).toBeFalsy();
-  });
+  describe("pwned extra cases", () => {
+    it("should handle pending", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "none" },
+          { type: "passwordGenerator/checkIfPwned/pending" }
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "pending" });
+    });
 
-  it("should react accordingly to check if pwned pending", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "none" },
-        { type: "passwordGenerator/checkIfPwned/pending" }
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "pending" });
-  });
+    it("should set result to bad if query is sucessful", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "none" },
+          { type: "passwordGenerator/checkIfPwned/fulfilled", payload: true }
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "bad" });
+    });
 
-  it("should set pwned result to bad if query is sucessful", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "none" },
-        { type: "passwordGenerator/checkIfPwned/fulfilled", payload: true }
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "bad" });
-  });
+    it("should set result to good if query is not sucessful", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "none" },
+          { type: "passwordGenerator/checkIfPwned/fulfilled", payload: false }
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "good" });
+    });
 
-  it("should set pwned result to good if query is not sucessful", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "none" },
-        { type: "passwordGenerator/checkIfPwned/fulfilled", payload: false }
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "good" });
-  });
+    it("should set result to error when something goes wrong", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "none" },
+          { type: "passwordGenerator/checkIfPwned/rejected" }
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "error" });
+    });
 
-  it("should set pwned result to error when something goes wrong", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "none" },
-        { type: "passwordGenerator/checkIfPwned/rejected" }
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "error" });
-  });
-
-  it("should reset pwned result back to none", () => {
-    expect(
-      passwordReducer(
-        { password: "old", copiedTimeout: 12, pwnedResult: "good" },
-        resetPwned()
-      )
-    ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "none" });
+    it("should handle resetting", () => {
+      expect(
+        passwordReducer(
+          { password: "old", copiedTimeout: 12, pwnedResult: "good" },
+          resetPwned()
+        )
+      ).toEqual({ password: "old", copiedTimeout: 12, pwnedResult: "none" });
+    });
   });
 });

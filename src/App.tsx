b7884c9f5963a52b216ae3bc70e2412ce333d33f
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-28 16:27:30 +0200

    feat: animate dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { Container, Paper, Stack, Typography } from "@mui/material";
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  Paper,
+  Stack,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { useAppSelector } from "./app/hooks";
 import AppBarSettings from "./features/appbar-settings";
+import { Settings } from "./features/appbar-settings/constants";
 import NotificationDisplay from "./features/notification";
 import PassConfig from "./features/pass-config";
 import PassGenerator from "./features/pass-generator";
@@ -17,33 +26,45 @@ const withDrawerStyle = {
 };
 
 const App = () => {
-  const settingsOpen = useAppSelector((state) => state.settings.settingsOpen);
+  const {
+    settingsOpen,
+    toggle: { [Settings.DarkTheme]: darkThemeToggled },
+  } = useAppSelector((state) => state.settings);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkThemeToggled ? "dark" : "light",
+    },
+  });
 
   return (
-    <Stack direction="column" sx={{ display: "flex" }}>
-      <AppBarSettings />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Stack direction="column" sx={{ display: "flex" }}>
+        <AppBarSettings />
 
-      <Container sx={baseStyle}>
-        <Stack
-          direction="column"
-          spacing={3}
-          sx={settingsOpen ? withDrawerStyle : {}}
-        >
-          <Paper elevation={2}>
-            <PassGenerator />
-          </Paper>
-          <Paper>
-            <PassConfig />
-          </Paper>
+        <Container sx={baseStyle}>
+          <Stack
+            direction="column"
+            spacing={3}
+            sx={settingsOpen ? withDrawerStyle : {}}
+          >
+            <Paper elevation={2}>
+              <PassGenerator />
+            </Paper>
+            <Paper>
+              <PassConfig />
+            </Paper>
 
-          <Typography variant="h5">
-            You should use a password manager such as...
-          </Typography>
-          <PassManagerPromotion />
-        </Stack>
-      </Container>
-      <NotificationDisplay />
-    </Stack>
+            <Typography variant="h5">
+              You should use a password manager such as...
+            </Typography>
+            <PassManagerPromotion />
+          </Stack>
+        </Container>
+        <NotificationDisplay />
+      </Stack>
+    </ThemeProvider>
   );
 };
 

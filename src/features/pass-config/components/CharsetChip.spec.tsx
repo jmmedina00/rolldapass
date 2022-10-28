06f34665161e07e69317fa6c494eef5a8c6bf420
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-28 15:37:42 +0200

    test(pass-config): test state reflection in charset chip

--- src/features/pass-config/components/CharsetChip.spec.tsx
+++ src/features/pass-config/components/CharsetChip.spec.tsx
@@ -2,6 +2,7 @@ import { act } from "react-dom/test-utils";
 import { setupStore } from "../../../app/store";
 import { renderWithProviders } from "../../../utils/test-utils";
 import { ConfigState, toggleCharset } from "../configSlice";
+import CharsetDefinition from "../constants";
 import CharsetChip from "./CharsetChip";
 
 describe("charset chip component", () => {
@@ -11,18 +12,21 @@ describe("charset chip component", () => {
     additionalChars: { include: "", exclude: "" },
   };
 
-  it("should dispatch a toggle charset action when clicked", () => {
-    const charset = "abc";
-    const label = "Chip test";
-    const category = "basic";
-    const chip = <CharsetChip {...{ label, charset, category }} />;
+  const chipInfo: CharsetDefinition = {
+    charset: "abc",
+    label: "Chip test",
+    category: "basic",
+  };
 
+  it("should dispatch a toggle charset action when clicked", () => {
     const expectedStore = setupStore({ config: baseConfig });
-    expectedStore.dispatch(toggleCharset({ charset, category }));
+    expectedStore.dispatch(toggleCharset({ ...chipInfo }));
 
     const actualStore = setupStore({ config: baseConfig });
-    const { getByText } = renderWithProviders(chip, { store: actualStore });
-    const renderedChip = getByText(label);
+    const { getByText } = renderWithProviders(<CharsetChip {...chipInfo} />, {
+      store: actualStore,
+    });
+    const renderedChip = getByText(chipInfo.label);
 
     act(() => {
       renderedChip.click();
@@ -30,4 +34,29 @@ describe("charset chip component", () => {
 
     expect(expectedStore.getState()).toEqual(actualStore.getState());
   });
+
+  it.each([
+    [true, "MuiChip-colorPrimary"],
+    [false, "MuiChip-colorDefault"],
+  ])(
+    "should reflect selected charset state in store",
+    (present, interestingClass) => {
+      const { getByText } = renderWithProviders(<CharsetChip {...chipInfo} />, {
+        preloadedState: {
+          config: {
+            length: 2,
+            charsets: {
+              basic: present ? [chipInfo.charset] : [],
+              advanced: [],
+            },
+            additionalChars: { include: "", exclude: "" },
+          },
+        },
+      });
+
+      const chip = getByText(chipInfo.label).parentElement as HTMLElement;
+
+      expect(Array.from(chip.classList)).toContain(interestingClass);
+    }
+  );
 });

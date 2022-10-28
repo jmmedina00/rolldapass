b779f2f0080741376bb0d25891e659eb12618d14
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-28 12:51:16 +0200

    test(pass-generator): reorganize tests for pass field

--- src/features/pass-generator/components/PassField.spec.tsx
+++ src/features/pass-generator/components/PassField.spec.tsx
@@ -18,7 +18,7 @@ jest.mock("../thunks/notifiedClipboard");
 jest.mock("../services/generate-password");
 jest.mock("../services/haveIBeenPwned"); // called by password slice thunk
 
-const unrelatedSlice = createSlice({
+/* const unrelatedSlice = createSlice({
   name: "unrelated",
   initialState: { counter: 1 },
   reducers: {
@@ -26,7 +26,7 @@ const unrelatedSlice = createSlice({
       state.counter += 1;
     },
   },
-});
+}); */
 
 describe("password generator", () => {
   const presetPassword = "abcd";
@@ -62,111 +62,135 @@ describe("password generator", () => {
     expect(getByDisplayValue(presetPassword)).toBeInTheDocument();
   });
 
-  it("should dispatch an action in store when password changes manually", () => {
-    const newPassword = "1234";
-    const expectedStore = setupStore();
-    expectedStore.dispatch(changePassword(newPassword));
+  describe("password changes", () => {
+    it("should dispatch an action in store when done manually", () => {
+      const newPassword = "1234";
+      const expectedStore = setupStore();
+      expectedStore.dispatch(changePassword(newPassword));
 
-    const actualStore = setupStore();
-    const { getByDisplayValue } = renderWithProviders(<PassField />, {
-      store: actualStore,
-    });
-    const input = getByDisplayValue("");
+      const actualStore = setupStore();
+      const { getByDisplayValue } = renderWithProviders(<PassField />, {
+        store: actualStore,
+      });
+      const input = getByDisplayValue("");
 
-    act(() => {
-      fireEvent.change(input, { target: { value: newPassword } });
+      act(() => {
+        fireEvent.change(input, { target: { value: newPassword } });
+      });
+      expect(expectedStore.getState()).toEqual(actualStore.getState());
     });
-    expect(expectedStore.getState()).toEqual(actualStore.getState());
+
+    it("should call for clipboard to be cleared", () => {
+      const store = setupStore({
+        config: {
+          length: 3,
+          charsets: { basic: ["ABCD"], advanced: [] },
+          additionalChars: { include: "", exclude: "" },
+        },
+        passwordGenerator: {
+          password: "old",
+          copiedTimeout: undefined,
+          pwnedResult: "none",
+        },
+      });
+
+      renderWithProviders(<PassField />, { store });
+      act(() => {
+        store.dispatch(changePassword("new"));
+      });
+
+      expect(clearClipboard).toHaveBeenCalled();
+    });
+
+    it("should reset pwned status", () => {
+      const expectedStore = setupStore({
+        passwordGenerator: {
+          password: "abcd",
+          copiedTimeout: undefined,
+          pwnedResult: "good",
+        },
+        settings: {
+          settingsOpen: false,
+          aboutOpen: false,
+          toggle: { [Settings.HaveIBeenPwned]: true },
+        },
+      });
+      expectedStore.dispatch(resetPwned());
+
+      const actualStore = setupStore({
+        passwordGenerator: {
+          password: "abcd",
+          copiedTimeout: undefined,
+          pwnedResult: "good",
+        },
+        settings: {
+          settingsOpen: false,
+          aboutOpen: false,
+          toggle: { [Settings.HaveIBeenPwned]: true },
+        },
+      });
+
+      renderWithProviders(<PassField />, { store: actualStore });
+
+      act(() => {
+        actualStore.dispatch(changePassword("qwerty"));
+      });
+
+      expect(expectedStore.getState().passwordGenerator.pwnedResult).toEqual(
+        actualStore.getState().passwordGenerator.pwnedResult
+      );
+    });
   });
 
-  it("should get a new password when config state changes", () => {
-    const store = setupStore({
-      config: {
-        length: 3,
-        charsets: { basic: ["ABCD"], advanced: ["1234"] },
-        additionalChars: { include: "&", exclude: "B" },
-      },
-      settings: {
-        settingsOpen: false,
-        aboutOpen: false,
-        toggle: { [Settings.AdvancedConfig]: false },
-      },
+  describe("config changes", () => {
+    it("should trigger getting a password from service", () => {
+      const store = setupStore({
+        config: {
+          length: 3,
+          charsets: { basic: ["ABCD"], advanced: ["1234"] },
+          additionalChars: { include: "&", exclude: "B" },
+        },
+        settings: {
+          settingsOpen: false,
+          aboutOpen: false,
+          toggle: { [Settings.AdvancedConfig]: false },
+        },
+      });
+
+      renderWithProviders(<PassField />, { store });
+
+      act(() => {
+        store.dispatch(changeLength(4));
+      });
+
+      const expectedCharsets = selectNormalizedCharsets(store.getState());
+
+      expect(generatePassword).toHaveBeenLastCalledWith(4, expectedCharsets);
     });
 
-    renderWithProviders(<PassField />, { store });
+    it("should dispatch change password action", () => {
+      (generatePassword as jest.Mock).mockReturnValue("test");
 
-    act(() => {
-      store.dispatch(changeLength(4));
+      const store = setupStore({
+        config: {
+          length: 3,
+          charsets: { basic: ["ABCD"], advanced: [] },
+          additionalChars: { include: "", exclude: "" },
+        },
+        passwordGenerator: {
+          password: "old",
+          copiedTimeout: undefined,
+          pwnedResult: "none",
+        },
+      });
+
+      renderWithProviders(<PassField />, { store });
+      act(() => {
+        store.dispatch(changeLength(4));
+      });
+
+      expect(store.getState().passwordGenerator.password).not.toEqual("old");
     });
-
-    const expectedCharsets = selectNormalizedCharsets(store.getState());
-
-    expect(generatePassword).toHaveBeenLastCalledWith(4, expectedCharsets);
-  });
-
-  it("should change the password in state when config state changes", () => {
-    (generatePassword as jest.Mock).mockReturnValue("test");
-
-    const store = setupStore({
-      config: {
-        length: 3,
-        charsets: { basic: ["ABCD"], advanced: [] },
-        additionalChars: { include: "", exclude: "" },
-      },
-      passwordGenerator: {
-        password: "old",
-        copiedTimeout: undefined,
-        pwnedResult: "none",
-      },
-    });
-
-    renderWithProviders(<PassField />, { store });
-    act(() => {
-      store.dispatch(changeLength(4));
-    });
-
-    expect(store.getState().passwordGenerator.password).not.toEqual("old");
-  });
-
-  it("should call clear clipboard thunk when the password changes", () => {
-    const store = setupStore({
-      config: {
-        length: 3,
-        charsets: { basic: ["ABCD"], advanced: [] },
-        additionalChars: { include: "", exclude: "" },
-      },
-      passwordGenerator: {
-        password: "old",
-        copiedTimeout: undefined,
-        pwnedResult: "none",
-      },
-    });
-
-    renderWithProviders(<PassField />, { store });
-    act(() => {
-      store.dispatch(changeLength(4));
-    });
-
-    expect(clearClipboard).toHaveBeenCalled();
-  });
-
-  it("should keep the current password in state when something unrelated in state changes", () => {
-    const store = configureStore({
-      reducer: combineReducers({
-        passwordGenerator: passwordReducer,
-        unrelated: unrelatedSlice.reducer,
-        config: configReducer,
-        settings: settingsReducer,
-      }),
-    });
-
-    renderWithProviders(<PassField />, { store });
-    act(() => {
-      store.dispatch(changePassword("thisisGood"));
-      store.dispatch(unrelatedSlice.actions.increaseCounter());
-    });
-
-    expect(store.getState().passwordGenerator.password).toEqual("thisisGood");
   });
 
   it("should send password to HIBP after timer", async () => {
@@ -193,44 +217,5 @@ describe("password generator", () => {
     jest.runAllTimers(); // testing-library doesn't like being updated by fake timers
 
     expect((checkPassword as jest.Mock).mock.lastCall[0]).toEqual("qwerty");
-  });
-
-  it("should reset pwned status when password changes", () => {
-    const expectedStore = setupStore({
-      passwordGenerator: {
-        password: "abcd",
-        copiedTimeout: undefined,
-        pwnedResult: "good",
-      },
-      settings: {
-        settingsOpen: false,
-        aboutOpen: false,
-        toggle: { [Settings.HaveIBeenPwned]: true },
-      },
-    });
-    expectedStore.dispatch(resetPwned());
-
-    const actualStore = setupStore({
-      passwordGenerator: {
-        password: "abcd",
-        copiedTimeout: undefined,
-        pwnedResult: "good",
-      },
-      settings: {
-        settingsOpen: false,
-        aboutOpen: false,
-        toggle: { [Settings.HaveIBeenPwned]: true },
-      },
-    });
-
-    renderWithProviders(<PassField />, { store: actualStore });
-
-    act(() => {
-      actualStore.dispatch(changePassword("qwerty"));
-    });
-
-    expect(expectedStore.getState().passwordGenerator.pwnedResult).toEqual(
-      actualStore.getState().passwordGenerator.pwnedResult
-    );
   });
 });

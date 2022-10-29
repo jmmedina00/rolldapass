5c0f90b577f03db09d9355cc2ef6744b114cc46a
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-29 18:34:03 +0200

    feat(pass-generator): hide entropy select unless it is enabled in settings

--- src/features/pass-generator/components/PassGenerator.tsx
+++ src/features/pass-generator/components/PassGenerator.tsx
@@ -1,15 +1,21 @@
 import { Box } from "@mui/material";
+import { useAppSelector } from "../../../app/hooks";
+import { Settings } from "../../appbar-settings/constants";
 import EntropySelect from "./EntropySelector";
 
 import PassEntropy from "./PassEntropy";
 import PassField from "./PassField";
 
 const PassGenerator = () => {
+  const entropyEnabled = useAppSelector(
+    (state) => state.settings.toggle[Settings.Entropy]
+  );
+
   return (
     <Box p={3}>
       <PassField />
       <PassEntropy />
-      <EntropySelect />
+      {entropyEnabled ? <EntropySelect /> : <div />}
     </Box>
   );
 };

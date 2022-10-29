8bfb18170011b316eddd6ffcea9b11b39680b730
Juan Miguel Medina Prieto <1a.medina.prieto.juanmiguel@gmail.com>
2022-10-29 13:38:33 +0200

    build: add i18n configuration, test it with pass-config

--- src/features/pass-config/components/PassConfig.tsx
+++ src/features/pass-config/components/PassConfig.tsx
@@ -8,22 +8,25 @@ import LengthSlider from "./LengthSlider";
 import CharFieldSet from "./CharFieldSet";
 import { useAppSelector } from "../../../app/hooks";
 import { Settings } from "../../appbar-settings/constants";
+import { useTranslation } from "react-i18next";
 
 const generateChips = (charsetDef: CharsetDefinition) => (
   <CharsetChip key={charsetDef.charset} {...charsetDef} />
 );
 
 const PassConfig = () => {
+  const { t } = useTranslation("config");
+
   const advancedConfig = useAppSelector(
     (state) => state.settings.toggle[Settings.AdvancedConfig]
   );
 
   return (
     <Box p={3}>
-      <Typography variant="h6">Password length</Typography>
+      <Typography variant="h6">{t("length")}</Typography>
       <LengthSlider />
 
-      <Typography variant="h6">Character sets</Typography>
+      <Typography variant="h6">{t("charsetsLabel")}</Typography>
       <Grid
         container
         columnSpacing={{ md: 2, xs: 1 }}
